e6ee85f919bc1eeff55b6fdea82a61bf10950ce8
chidibede <chidibede@gmail.com>
2020-07-23 19:47:39 +0100

    Basic Node Server without express

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # WeJapa-Internship-Task1
+
+### Task Instruction
 Setup a Basic Node Server without express framework. The server returns Hello World, welcome to WeJapa Internships when '/' is navigated to
+
+# How to test the server
+clone this repository "https://github.com/chidibede/WeJapa-Internship-Task1.git" and open the project cloned in your favorite text editor and enter the command below on your terminal pointing to this project
+```bash
+npm start
+```
---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require("http");
+
+const returnStatement = "Hello World, welcome to WeJapa Internships";
+const server = http.createServer((req, res) => {
+    if (req.url === '/'){
+        res.writeHead(200, { "content-type": "text/plain" });
+        res.write(returnStatement);
+        res.end();
+    } else {
+        res.writeHead(404, { "content-type": "text/plain" });
+        res.write("Page not Found");
+        res.end();
+    }
+  
+});
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, (error) => {
+  if (error) {
+    console.log("Error encountered while trying to serve the app", error);
+  } else {
+    console.log(`Server listening on localhost:${port}`);
+  }
+});
